f45dfab18b2cf3bd91431a5fa73ad9d4e8227428
Andrey <adkurgin@sberbank.ru>
2022-02-11 17:20:51 +0300

    woohoo

--- src/util/index.js
+++ src/util/index.js
@@ -58,6 +58,7 @@ export const uploadNewProductsExcel = async (file, sha, apiToken) => {
 }
 
 export const createGuid = () => {
+
     function _p8(s) {
         const p = (Math.random().toString(16) + "000000000").substr(2, 8);
         return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
